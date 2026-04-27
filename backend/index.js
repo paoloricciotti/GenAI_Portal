5f0c9f15254507b4087e3b993b2f754618dffc73
Napoleoni <marco.napoleoni@accenture.com>
2026-04-27 20:14:47 +0200

    cambio https

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
+    'https://genai-portal.onrender.com'
+  ],
   credentials: true
 }));
 
